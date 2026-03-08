8d39f1933aad4a588e871b707cc0042fc8992d6d
MateoGuayasamin <giuseppe.guayasamin.2002@gmail.com>
2026-03-07 22:22:03 -0500

    project done

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        int PromptUserNumber()
+        {
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            return number;
+        }
+        int SquareNumber (int number)
+        {
+            number = number * number;
+            return number;
+        }
+
+        DisplayWelcome();
+        string UserName =PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squared = SquareNumber(userNumber);
+        Console.WriteLine($"{UserName}, the square of your number is {squared}");
     }
 }
