5ab6ceb5007a1f2f697942d22845ca4a5be4507d
MateoGuayasamin <giuseppe.guayasamin.2002@gmail.com>
2026-04-14 21:25:14 -0500

    w7

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
-    }
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("03 Nov 2022", 30, 5.0));
+        activities.Add(new Cycling("03 Nov 2022", 30, 20.0));
+        activities.Add(new Swimming("03 Nov 2022", 30, 40));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }    
 }
+    
+        
+
+// Base cl
+
+// Swimming class
